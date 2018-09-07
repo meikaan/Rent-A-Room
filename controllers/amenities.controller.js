1961eed0ab5b055c68a3d6358f7d888fdb00f87e
sharanchavadi <sharanschavadi@gmail.com>
2018-09-07 13:30:53 +0530

    Updated aminity model+controller+routes+spec

---
+++ controllers/amenities.controller.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Amenity = require('../models/amenity.model');
+
+module.exports = class AmenityController {
+
+    create(req, res) {
+        return Amenity.create(req.body)
+            .then(result => res.send(result));
+    }
+
+    index(req, res) {
+        return Amenity.find({})
+            .then(result => res.send(result));
+    }
+
+    read(req, res) {
+        return Amenity.findById(req.params.id)
+            .then(result => res.send(result));
+    }
+
+    delete(req, res) {
+        return Amenity.findByIdAndRemove(req.params.id)
+            .then(result => res.send(result));
+    }
+
+    _update(id, data) {
+        return Amenity.findByIdAndUpdate(id, {$set: data})
+            .then(result => {
+                    return Amenity.findById(id).then(amenity => {
+                        return amenity;
+                    })
+                })
+    }
+
+    update(req, res) {
+        this._update(req.params.id, req.body).then(updatedAmenity => {
+            res.send(updatedAmenity);
+        });
+    }
+
+}
---
+++ routes/amenity.route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const AmenitiesController = require('../controllers/amenities.controller');
+
+let amenityController = new AmenitiesController();
+
+router.post('/', (...args) => amenityController.create(...args));
+router.get('/:id', (...args) => amenityController.read(...args));
+router.put('/:id', (...args) => amenityController.update(...args));
+router.delete('/:id', (...args) => amenityController.delete(...args));
+router.get('/', (...args) => amenityController.index(...args));
+
+module.exports = router;
