83d0a4100b89d15aff87cf9ce9b10840c6907b1a
sharanchavadi <sharanschavadi@gmail.com>
2018-09-22 17:31:56 +0530

    Fixed previous specs+added additional validation specs for price, startDate and endDate

--- spec/controllers/specialPrices.controller.spec.js
+++ spec/controllers/specialPrices.controller.spec.js
@@ -7,26 +7,38 @@ let connection = mongoose.connect('mongodb://localhost/rentaroom_test');
 const Schema = mongoose.Schema;
 
 let SpecialPriceSchema = new Schema({
-    price: {type: Number, required: true},
-    startDate: {type: Date, required: true},
-    endDate: {type: Date, required: true}  
+    price: {
+    	type: Number, 
+    	required: true, 
+    	min: 100,
+    	max: 5000,
+    	//Todo
+    	//validate: {validator: Number.isInteger, message: '{VALUE} is not an integer'}
+    },
+    startDate: {
+    	type: Date, 
+    	required: true 
+    },
+    endDate: {
+    	type: Date, 
+    	required: true, 
+    	validate: [dateValidator, 'startDate must be less than endDate']
+    }  
 });
-SpecialPrice = mongoose.model('SpecialPrice', SpecialPriceSchema);
+
+const SpecialPrice = mongoose.model('SpecialPrice', SpecialPriceSchema);
+
+// function that validate the startDate and endDate
+function dateValidator(endDate){
+	// `this` is the mongoose document
+	return this.startDate < endDate;
+}
 
 class SpecialPricesController{
 	create(req, res) {
         return SpecialPrice.create(req.body)
             .then(result => res.send(result));
     }
-}
-
-//Function to convert given date into mm/dd/yyyy format
-function formatDate(date) {
-    var givenDate = date;
-    var month = givenDate.getMonth() + 1;
-    var day = givenDate.getDate();
-    var year = givenDate.getFullYear();
-    return month + "/" + day + "/" + year;
 }
 
 describe('SpecialPricesController', () => {
@@ -40,6 +52,7 @@ describe('SpecialPricesController', () => {
     describe('Create', () => {
 		
 		it('should create record with all the fields', () => {
+			
 			let data = {
 				price: 1500,
 				startDate: new Date("2018-10-27"),
@@ -61,8 +74,8 @@ describe('SpecialPricesController', () => {
 			
 			let data = {
 				price: 1500,
-				startDate: new Date("2018-10-27"),
-				endDate: new Date("2018-10-30")
+				startDate: "2018-10-27",
+				endDate: "2018-10-30"
 			};
 
 			let fields = ["price", "startDate", "endDate"];
@@ -75,34 +88,34 @@ describe('SpecialPricesController', () => {
 				    fail("create shouldn't pass");}).catch(e => {
 					expect(e.name).toEqual("ValidationError"); 
 					expect(e.errors[field].message).toEqual(`Path \`${field}\` is required.`);
-					//UnhandledPromiseRejectionWarning: ValidationError: SpecialPrice validation failed: price: Path `price` is required.
 				});
 			});
 		});
 
-		it('should create record if price field length is less than specified limit', () => {
+		it('should not create record if price is greater than specified limit', () => {
 
 			let data = {
-				price: 1500,
-				startDate: new Date("2018-10-27"),
-				endDate: new Date("2018-10-30")
+				price: 6000,
+				startDate: "2018-10-27",
+				endDate: "2018-10-30"
 			};
 
 			let specialPricesController = new SpecialPricesController();
 
 			return specialPricesController.create({body: data}, res).then(result => {
-				return SpecialPrice.findOne(data).then(newSpecialPrice => {
-					expect(newSpecialPrice["price"].toString().length).toBeLessThanOrEqual(4);
-				});
+				fail("create shouldn't pass");}).catch(e => {
+					expect(e.name).toEqual("ValidationError"); 
+					expect(e.errors["price"].message).toEqual(`Path \`price\` (${data["price"]}) is more than maximum allowed value (5000).`); 
 			});
 		});
 
+		//revisit this one
 		it('should create record if price is numerical', () => {
 
 			let data = {
 				price: 1500,
-				startDate: new Date("2018-10-27"),
-				endDate: new Date("2018-10-30")
+				startDate: "2018-10-27",
+				endDate: "2018-10-30"
 			};
 
 			let specialPricesController = new SpecialPricesController();
@@ -118,8 +131,8 @@ describe('SpecialPricesController', () => {
 
 			let data = {
 				price: 1500,
-				startDate: new Date("2018-10-27"),
-				endDate: new Date("2018-10-30")
+				startDate: "2018-10-27",
+				endDate: "2018-10-30"
 			};
 
 			let specialPricesController = new SpecialPricesController();
@@ -131,40 +144,75 @@ describe('SpecialPricesController', () => {
 			});
 		});
 
-		it('should create record if startDate and endDate format is MM/DD/YY', () => {
-
+		it('should not create record if price is not numerical', () => {
+			
 			let data = {
-				price: 1500,
-				startDate: new Date("2018-10-27"),
-				endDate: new Date("2018-10-30")
+				price: "xyz",
+				startDate: "2018-10-27",
+				endDate: "2018-10-30"
 			};
 
 			let specialPricesController = new SpecialPricesController();
-
+			
 			return specialPricesController.create({body: data}, res).then(result => {
-				return SpecialPrice.findOne(data).then(newSpecialPrice => {
-					expect(formatDate(newSpecialPrice["startDate"])).toEqual("10/27/2018");
-					expect(formatDate(newSpecialPrice["endDate"])).toEqual("10/30/2018");
-				});
+				fail("create shouldn't pass");}).catch(e => { 
+					expect(e.name).toEqual("ValidationError"); 
+					expect(e.errors["price"].message).toEqual(`Cast to Number failed for value "${data["price"]}" at path "price"`);
+			});
+		
+		});
+
+		it('should not create record if price is less than specified limit', () => {
+			
+			let data = {
+				price: 0,
+				startDate: "2018-10-27",
+				endDate: "2018-10-30"
+			};
+
+			let specialPricesController = new SpecialPricesController();
+			
+			return specialPricesController.create({body: data}, res).then(result => {
+				fail("create shouldn't pass");}).catch(e => { 
+					expect(e.name).toEqual("ValidationError"); 
+					expect(e.errors["price"].message).toEqual(`Path \`price\` (${data["price"]}) is less than minimum allowed value (100).`); 
+
+			});
+		});
+		
+		it('should not create record if startDate is greater than or equals to endDate', () => {
+			
+			let data = {
+				price: 1500,
+				startDate: "2018-10-30",
+				endDate: "2018-10-27"
+			};
+
+			let specialPricesController = new SpecialPricesController();
+			
+			return specialPricesController.create({body: data}, res).then(result => {
+				fail("create shouldn't pass");}).catch(e => { 
+					expect(e.name).toEqual("ValidationError"); 
+					expect(e.errors["endDate"].message).toEqual('startDate must be less than endDate');
 			});
 		});
 
-		it('should not create record if price is not numerical', () => {
+		it('should not create record if startDate and endDate is invalid', () => {
 
+			let data = {
+				price: 1500,
+				startDate: "xyz",
+				endDate: "xyz"
+			};
+
+			let specialPricesController = new SpecialPricesController();
+			
+			return specialPricesController.create({body: data}, res).then(result => {
+				fail("create shouldn't pass");}).catch(e => {
+					expect(e.name).toEqual("ValidationError"); 
+					expect(e.errors["startDate"].message).toEqual(`Cast to Date failed for value "${data["startDate"]}" at path "startDate"`);
+					expect(e.errors["endDate"].message).toEqual(`Cast to Date failed for value "${data["endDate"]}" at path "endDate"`);
+			});
 		});
-
-		xit('should not create record if price field length is zero', () => {
-
-		});
-		
-		xit('should not create record if startDate is greater than or equals to endDate', () => {
-
-		});
-
-		xit('should not create record if startDate and endDate format is not MM/DD/YY', () => {
-
-		});
-
 	});
-
 });
